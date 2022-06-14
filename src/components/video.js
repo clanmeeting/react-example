5362b8f13a205ed2ae097521f8ac320a3ad7d441
hafizadil123 <adil-sikandar-01>
2022-06-15 00:41:00 +0500

    fix few things

--- src/components/video.js
+++ src/components/video.js
@@ -1,40 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import {clanMeeting} from 'react-clan-meeting';
-const VideoConferencing = ({ domain, consumerId, jwt }) => {
-    const [wi, setWi] = useState(null);
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.src = `https://${domain}/external_api.js`;
-        script.async = true;
-        script.onload = () => scriptLoaded();
+import React, { useEffect } from "react";
+import { clanMeeting } from "react-clan-meeting";
 
-        document.body.appendChild(script);
-    }, [domain, consumerId, jwt]);
-    const scriptLoaded = () => {
-        setWi(window);
-    }
+const VideoConferencing = ({ domain, consumerId, optionalProperties }) => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = `https://${domain}/external_api.js`;
+    script.async = true;
+    script.onload = () => scriptLoaded();
 
-    useEffect(() => {
-        const optionalProperties = {
-            roomName: 'uniqueRoomName',
-            displayName: 'Host',
-            jwt: jwt,
-            enableJoinMeetingPage: true,
+    document.body.appendChild(script);
+  }, [domain, consumerId, optionalProperties.jwt]);
+  const scriptLoaded = () => {
+    const meeting = new clanMeeting(domain, consumerId, optionalProperties);
+    meeting.generateRoomName().anonymizeDisplayName();
+    meeting.start();
+  };
 
-        };
-        const meeting = new clanMeeting(domain, consumerId, optionalProperties);
-        meeting.generateRoomName().anonymizeDisplayName();
-        meeting.start();
-    }, [wi]);
-    return (
-        <>
-            <span style={{
-                display: 'flex',
-                justifyContent: 'center'
-            }}>please wait....</span>
-        </>
-    );
-}
-
+  return (
+    <>
+      <span
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        please wait....
+      </span>
+    </>
+  );
+};
 
 export default VideoConferencing;
